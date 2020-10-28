ee49318c4db9cc8278c9ff54a324fdc590d6183e
TheAverageJoe291 <73551343+TheAverageJoe291@users.noreply.github.com>
2020-10-28 01:00:33 -0400

    Update install.sh
    Not Working

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-cd ~
-cd Downloads
+cd ~/Downloads
 echo "Please go to https://drive.google.com/u/0/uc?id=1lE_qzJbnR56Pk83gZH066Oq28b6_xNq3&export=download and download the .zip file, or download the game from anuke.itch.io/Mindustry."
 echo "It's going to say it can't check for viruses with the drive link, if you feel uneasy, use the other link from the game's author, both are safe."
 echo "It's just personally easier to do the drive link..."
@@ -8,8 +7,7 @@ read -n1 -r -p "Press any key to continue..." key
 unzip "mindustry-linux-64-bit-stable.zip"
 sleep 3
 mv "[Linux-64bit][v104.10]Mindustry" "mindustry"
-cd ~
-cd Downloads
+cd ~/Downloads
 mv "mindustry" "~/Desktop"
 rm  -R "~/Downloads/mindustry"
 echo "Mindustry is in the Mindustry folder"
