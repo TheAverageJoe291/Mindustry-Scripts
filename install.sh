243c2dd691b327e50389f94f96d16470eb82361b
TheAverageJoe291 <73551343+TheAverageJoe291@users.noreply.github.com>
2020-10-28 00:41:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,8 @@ read -n1 -r -p "Press any key to continue..." key
 unzip "mindustry-linux-64-bit-stable.zip"
 sleep 3
 mv "[Linux-64bit][v104.10]Mindustry" "mindustry"
-cd ~/Downloads
+cd "~/Downloads"
 mv "mindustry" "~/Desktop"
-rm "~/Downloads/mindustry-linux-64-bit-stable.zip"
 rm  -R "~/Downloads/mindustry"
 echo "Mindustry is in the Mindustry folder"
 sleep 5
